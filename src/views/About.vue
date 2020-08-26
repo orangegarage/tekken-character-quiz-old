79ae402c4491eb2e5c1bf38f97f001b572484219
Jin <jjjinyong@naver.com>
2020-08-26 19:52:27 -0400

    roll credits

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
-    <h1>About</h1>
+    <h1>Special thanks to...</h1>
+    <h3>bigcookie, jayelly, Merv, Kane, AStr8pro</h3>
+    <p>...and you!</p>
   </div>
 </template>
