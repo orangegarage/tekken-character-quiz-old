eaf65b95b3b9cf98108696ac1de751dd511941a2
Jin <jjjinyong@naver.com>
2020-08-28 21:02:37 -0400

    back to normal

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ module.exports = {
     lintOnSave: false,
     productionSourceMap: false,
     publicPath: process.env.NODE_ENV === 'production'
-    ? '/my-project/'
+    ? '/tekken-character-quiz/'
     : '/'
 };
