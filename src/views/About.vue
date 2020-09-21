19196ba57a07f91b30a995c9a2a67380946f2ef5
Jin <jjjinyong@naver.com>
2020-09-21 10:49:03 -0400

    rel noopener

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,12 +4,12 @@
     <div class="hello">
       <h3>{{$t('about.resources')}}:</h3>
       <ul>
-        <li><a href="https://www.reddit.com/r/Tekken/comments/699xxr/general_information_faq/">English</a></li>
-        <li><a href="https://youtu.be/Szm5O3lcy_U">Video(basic)</a></li>
-        <li><a href="https://www.youtube.com/playlist?list=PLVUxFFfCtDtkd3n0J8taq-MknCWaP6naW">Video(intermediate)</a></li>
-        <li><a href="https://gall.dcinside.com/m/tekken7/73843">한국어</a></li>
-        <li><a href="https://www.youtube.com/playlist?list=PLfw7Kerp114hdIQ9qQ2FlM7tsZZRxI_xz">영상 (기초)</a></li>
-        <li><a href="https://www.youtube.com/playlist?list=PLHsOA3vkFzzY91ni3X_-pF847EcL85UNz">영상 (캐릭터별)</a></li>
+        <li><a href="https://www.reddit.com/r/Tekken/comments/699xxr/general_information_faq/" target="_blank" rel="noopener">English</a></li>
+        <li><a href="https://youtu.be/Szm5O3lcy_U" target="_blank" rel="noopener">Video(basic)</a></li>
+        <li><a href="https://www.youtube.com/playlist?list=PLVUxFFfCtDtkd3n0J8taq-MknCWaP6naW" target="_blank" rel="noopener">Video(intermediate)</a></li>
+        <li><a href="https://gall.dcinside.com/m/tekken7/73843" target="_blank" rel="noopener">한국어</a></li>
+        <li><a href="https://www.youtube.com/playlist?list=PLfw7Kerp114hdIQ9qQ2FlM7tsZZRxI_xz" target="_blank" rel="noopener">영상 (기초)</a></li>
+        <li><a href="https://www.youtube.com/playlist?list=PLHsOA3vkFzzY91ni3X_-pF847EcL85UNz" target="_blank" rel="noopener">영상 (캐릭터별)</a></li>
       </ul>
       
       <h3>{{$t('about.stack')}}:</h3>
@@ -23,7 +23,7 @@
 
     <div class= "specialthanks">
     <h1>Special thanks to...</h1>
-    <h3><a href = "https://github.com/biggestcookie" target="_blank">bigcookie</a> | <a href= "https://twitter.com/genafurly" target="_blank">GenaFurLy</a> | <a href= "https://www.youtube.com/channel/UCGbtD4EH3UOmJR2AtjHr0kA" target="_blank">LoseAgainMan(또졌다맨)</a> | <a href= "https://www.youtube.com/channel/UCykrLI6gCNczfhY6pBEI9QA">Merv202</a> | <a href= "https://twitter.com/tekkenchanel">ROX_Chanel(샤넬)</a></h3>
+    <h3><a href = "https://github.com/biggestcookie" target="_blank" rel="noopener">bigcookie</a> | <a href= "https://twitter.com/genafurly" target="_blank" rel="noopener">GenaFurLy</a> | <a href= "https://www.youtube.com/channel/UCGbtD4EH3UOmJR2AtjHr0kA" target="_blank" rel="noopener">LoseAgainMan(또졌다맨)</a> | <a href= "https://www.youtube.com/channel/UCykrLI6gCNczfhY6pBEI9QA" rel="noopener">Merv202</a> | <a href= "https://twitter.com/tekkenchanel">ROX_Chanel(샤넬)</a></h3>
     <h4>WhoAmI, 네간리얼쑨, 리틀브로, Kane, AStr8pro</h4>
     <p>...and you!</p>
     
@@ -42,7 +42,7 @@
       <p>{{$t('about.answer5')}}</p> 
       <h3>6. {{$t('about.faq6')}}</h3>
       <p>{{$t('about.answer6')}}</p>
-      <h3><a href= "https://medium.com/@jjjinyong/vue-js-vue-cli-making-recommendations-based-on-quiz-questions-64f829aa3e02">{{$t('about.thanks')}}</a></h3>
+      <h3><a href= "https://medium.com/@jjjinyong/vue-js-vue-cli-making-recommendations-based-on-quiz-questions-64f829aa3e02" target="_blank" rel="noopener">{{$t('about.thanks')}}</a></h3>
     </div>
 
   </div>
